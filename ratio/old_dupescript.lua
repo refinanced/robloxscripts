be3d31452595fb82d7895c4f6bc7dcc93a982463
supa <137343463+refinanced@users.noreply.github.com>
2023-06-21 18:03:34 -0400

    Create old_dupescript.lua

---
+++ ratio/old_dupescript.lua
@@ -0,0 +1,10 @@
+-- the infamous dupe script!! pwned by unicode jaja
+
+local ohBoolean1 = "\255"
+local ohBoolean2 = "\255"
+local ohBoolean3 = "\128"
+local ohBoolean4 = "\128"
+local ohBoolean5 = "\128"
+local ohBoolean6 = "\255"
+
+game:GetService("ReplicatedStorage").Remotes.SetSettings:FireServer(ohBoolean1, ohBoolean2, ohBoolean3, ohBoolean4, ohBoolean5, ohBoolean6)
